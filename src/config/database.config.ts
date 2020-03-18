6fe0fc33f00a239cf999898337f36d1d18a24974
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-03-18 19:06:34 +0000

    chore: database deployment with docker

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,4 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('database', () => ({
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT || 5432,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    name: process.env.POSTGRES_DB
 }));
