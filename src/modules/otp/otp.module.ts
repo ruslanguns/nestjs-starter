f9433d28814e01db88d4fdab5e3070453f3ea4d2
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-03-22 14:50:31 +0000

    chore: unnecesary controller

--- src/modules/otp/otp.module.ts
+++ src/modules/otp/otp.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { EmailOtpService } from './email-otp/email-otp.service';
 import { SmsOtpService } from './sms-otp/sms-otp.service';
-import { OtpController } from './otp.controller';
 
 @Module({
-  providers: [EmailOtpService, SmsOtpService],
-  controllers: [OtpController]
+    providers: [EmailOtpService, SmsOtpService],
 })
 export class OtpModule { }
