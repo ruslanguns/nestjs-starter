feadfd209262282d3f55481d36829159ea3d102f
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-03-22 18:50:15 +0000

    feat(core): scheduler module for tasks

---
+++ src/core/scheduler/scheduler.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TaskService } from './task.service';
+
+@Module({
+    imports: [ScheduleModule.forRoot()],
+    providers: [TaskService],
+})
+export class SchedulerModule { }
---
+++ src/core/scheduler/task.service.ts
@@ -0,0 +1,12 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+
+@Injectable()
+export class TaskService {
+    private readonly logger = new Logger(TaskService.name);
+
+    @Cron(CronExpression.EVERY_30_MINUTES)
+    handleCron() {
+        this.logger.debug('Server is alive!');
+    }
+}
