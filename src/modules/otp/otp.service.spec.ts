27e680c0e8e7842e6cbf0200819e15ea3c8c25c4
Ruslan Gonzalez <ruslanguns@gmail.com>
2020-03-23 12:40:44 +0000

    feat(otp): added service otp

---
+++ src/modules/otp/otp.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { OtpService } from './otp.service';
+
+describe('OtpService', () => {
+  let service: OtpService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [OtpService],
+    }).compile();
+
+    service = module.get<OtpService>(OtpService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
